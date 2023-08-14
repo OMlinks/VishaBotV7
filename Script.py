92167874babf7f0b32ab936af1ca129d31c45431
Soman <127747278+Soman-sir@users.noreply.github.com>
2023-08-14 14:58:59 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -9,7 +9,7 @@ class script(object):
 # ⚠️ Please don't change our credits 👇🏻
 
     ABOUT_TXT = """<b>✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Malayali_No2>✑ മലയꫂള ᭄℡𓆈</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Malayali_No2>✑ മലയꫂള ᭄℡</a>
 ✯ 𝚄𝙿𝙳𝙰𝚃𝙴𝚂: <a href=https://t.me/P_H_Movies>Tᴏᴜᴄʜ</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
