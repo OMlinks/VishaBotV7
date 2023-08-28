c7c22fb1d66b19f3672bde2ca144998579171799
Soman <127747278+Soman-sir@users.noreply.github.com>
2023-08-28 09:02:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -114,7 +114,7 @@ async def next_page(bot, query):
             btn = [
                 [
                     InlineKeyboardButton(
-                        text=f"[🪰{get_size(file.file_size)}🪰] {file.file_name}", url=await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
+                        text=f"[{🪰get_size(file.file_size)🪰}] {file.file_name}", url=await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
                     ),
                 ]
                 for file in files
@@ -137,7 +137,7 @@ async def next_page(bot, query):
             btn = [
                 [
                     InlineKeyboardButton(
-                        text=f"[🪰{get_size(file.file_size)}🪰] {file.file_name}", callback_data=f'files#{file.file_id}'
+                        text=f"[{🪰get_size(file.file_size)🪰}] {file.file_name}", callback_data=f'files#{file.file_id}'
                     ),
                 ]
                 for file in files
@@ -727,7 +727,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "surprise":
         btn = [[
-            InlineKeyboardButton('🪄sᴜʀᴘʀɪsᴇ🪄', callback_data='start')
+            InlineKeyboardButton('👛sᴜʀᴘʀɪsᴇ👛', callback_data='start')
         ]]
         reply_markup=InlineKeyboardMarkup(btn)
         await client.edit_message_media(
@@ -1299,7 +1299,7 @@ async def auto_filter(client, msg, spoll=False):
             btn = [
                 [
                     InlineKeyboardButton(
-                        text=f"[🪰{get_size(file.file_size)}🪰] {file.file_name}", url=await get_shortlink(message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
+                        text=f"[{🪰get_size(file.file_size)🪰}] {file.file_name}", url=await get_shortlink(message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
                     ),
                 ]
                 for file in files
@@ -1323,7 +1323,7 @@ async def auto_filter(client, msg, spoll=False):
             btn = [
                 [
                     InlineKeyboardButton(
-                        text=f"[🪰{get_size(file.file_size)}🪰] {file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                        text=f"[{🪰get_size(file.file_size)🪰}] {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                     ),
                 ]
                 for file in files
